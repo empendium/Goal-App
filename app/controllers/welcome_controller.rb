f1d3a1242a5ea2b2e1343ad4eb0c1aa23a18fe95
Emily <empendium@gmail.com>
2011-12-13 22:32:12 -0600

    added related comments to home view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,6 +10,18 @@ class WelcomeController < ApplicationController
     @comment = Comment.new
     @user = User.find(session[:uid])
     @current_goal = @user.goals.first
+    
+    @related_milestones = Milestone.joins(:goal)
+      .where('goals.category' => @current_goal.category)
+      .where('goals.id !=?', @current_goal.id)
+    
+    @related_milestones_ids = @related_milestones.map {|milestone| milestone.id }
+    @related_comments = Comment.where(milestone_id: @related_milestones_ids)
+        .order('created_at DESC')
+        .where(:created_at => (Time.now - 5.days)...(Time.now))
+        .limit(10)
+        
+        
     @milestones = @current_goal.milestones.to_a
     @milestone_1 = @milestones[0]
     @milestone_2 = @milestones[1]
@@ -23,8 +35,10 @@ class WelcomeController < ApplicationController
     if @milestone_3.nil?
       @milestone_3 = Milestone.new(description:"Create a Milestone")
     end
+    @comments = Comment.where(:milestone_id => [@milestone_1.id, @milestone_2.id, @milestone_3.id])
+      .order('created_at DESC')
+      .where(:created_at => (Time.now - 5.days)...(Time.now))
   end
-  
 end
 
 
